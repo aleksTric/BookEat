7c364657d7c9d4a2aea62084582298f854a90b57
aleksTric <121251751+aleksTric@users.noreply.github.com>
2024-05-23 16:04:45 +0300

    Add files via upload

---
+++ search_friends/build/searchfriends.py
@@ -0,0 +1,39 @@
+from database import Database 
+
+class SearchFriends:
+    def __init__(self, canvas, db, text_item0, text_item1, text_item2, text_item3, text_item4, text_item5):
+         self.canvas = canvas
+         self.db = db
+         self.text_item0 = text_item0
+         self.text_item1 = text_item1
+         self.text_item2 = text_item2
+         self.text_item3 = text_item3
+         self.text_item4 = text_item4
+         self.text_item5 = text_item5
+
+    def search(self, search_term):
+            
+            query = "SELECT username FROM account WHERE username LIKE %s"
+            cursor = self.db.execute_query(query, ('%' + search_term + '%',))
+            rows = cursor.fetchall()
+            
+            self.canvas.itemconfig(self.text_item0, text="")
+            self.canvas.itemconfig(self.text_item1, text="")
+            self.canvas.itemconfig(self.text_item2, text="")
+            self.canvas.itemconfig(self.text_item3, text="")
+            self.canvas.itemconfig(self.text_item4, text="")
+            self.canvas.itemconfig(self.text_item5, text="")
+
+            self.canvas.itemconfig(self.text_item0, text=rows[0])
+            self.canvas.itemconfig(self.text_item1, text=rows[1])
+            self.canvas.itemconfig(self.text_item2, text=rows[2])
+            self.canvas.itemconfig(self.text_item3, text=rows[3])
+            self.canvas.itemconfig(self.text_item4, text=rows[4])
+            self.canvas.itemconfig(self.text_item5, text=rows[5])
+
+
+            #for i,row in enumerate(rows):
+            #    if i<3:
+            #        self.canvas.itemconfig(self.text_items_row1[i], text=rows[i])
+            #    elif i>=3 and i<6:
+            #        self.canvas.itemconfig(self.text_items_row2[i-3], text=rows[i])
