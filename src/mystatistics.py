cc14e9a2e336c9d5aee370c85c952af9daf3b542
jimbaroutis <jimserfarei@gmail.com>
2024-05-26 13:55:58 +0300

    define Statisics

---
+++ src/mystatistics.py
@@ -0,0 +1,16 @@
+
+class Statistics:
+
+    def __init__(self, requested_books, latest_books,popular_books):
+        self.__requested_books = requested_books
+        self.__latest_books=latest_books
+        self.__popular_books = popular_books
+        
+    def estimate():
+        pass
+    
+    def display():
+        pass
+
+    def get_requested_books():
+        pass
