06c38a33408090dd7cbcca674074193a7ca5cd7c
aleksTric <121251751+aleksTric@users.noreply.github.com>
2024-05-30 21:24:59 +0300

    Delete friendslist.py

--- friendslist.py
+++
@@ -1,9 +0,0 @@
-class FriendsList:
-   def __init__ (self, canvas, db, sender_id, receiver_id, status):
-      self.canvas = canvas 
-      self.db = db
-      self.sender_id = sender_id
-      self.receiver_id = receiver_id
-
-   #def add_friend():
-      
